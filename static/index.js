70518d3eb95266bf138631cd9e57f54e535bf2b7
Jigar1717 <124237200+Jigar1717@users.noreply.github.com>
2023-02-28 21:29:35 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,10 @@
+
+function f1() {
+    // let submit = document.getElementById('submit');
+    window.location.href = "index.pug";
+    // console.log("Hello...");
+
+    // submit.addEventListener('click', ()=> {
+    // });
+
+}
